4a8105b1b83d5b758f36916547bc7d3ab0e121b4
Paola Kossa <paolakossa@gmail.com>
2022-11-23 10:19:33 -0300

    Final project

---
+++ projeto/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Comment/>
+</template>
+
+<script>
+import Comment from './components/Comment'
+
+export default {
+  name: 'App',
+  components: {
+    Comment
+  }
+}
+</script>
+
+<style>
+
+
+</style>
